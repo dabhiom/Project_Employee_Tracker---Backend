a953742339ae6e134562ba28068fb63ea29e0d9c
dabhiom <dabhiom52@gmail.com>
2026-03-16 12:24:01 +0530

    ddl.routes upadted

--- routes/ddl.routes.js
+++ routes/ddl.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getClientDdls, getEmployeeDdls, getDesignationDdls } = require('../controllers/ddl.controller');
-const { protect } = require('../middleware/auth.middleware');
+const { verifyToken } = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
-router.use(protect); // Ensure all DDL routes require authentication
+router.use(verifyToken); // Ensure all DDL routes require authentication
 
 /**
  * @swagger
